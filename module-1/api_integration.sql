2327de9d5945273f406d168c29e317023d6a0092
Volodya Byelobrov <volodia.byelobrov@gmail>
2026-04-03 12:49:48 +0200

    api id updates

--- module-1/api_integration.sql
+++ module-1/api_integration.sql
@@ -5,20 +5,20 @@ USE SCHEMA public;
 -- Create credentials
 CREATE OR REPLACE SECRET course_repo.public.github_pat
   TYPE = password
-  USERNAME = ''
+  USERNAME = 'sh-am-si'
   PASSWORD = '';
 
 -- Create the API integration
 CREATE OR REPLACE API INTEGRATION
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('') -- URL to your GitHub profile
+  API_ALLOWED_PREFIXES = ('https://github.com/sh-am-si') -- URL to your GitHub profile
   ALLOWED_AUTHENTICATION_SECRETS = ()
   ENABLED = TRUE;
 
 -- Create the git repository object
 CREATE OR REPLACE GIT REPOSITORY course_repo.public.advanced_data_engineering_snowflake
   API_INTEGRATION =  -- Name of the API integration defined above
-  ORIGIN = '' -- Insert URL of forked repo
+  ORIGIN = 'https://github.com/sh-am-si/advanced-data-engineering-snowflake.git' -- Insert URL of forked repo
   GIT_CREDENTIALS = ;
 
 -- List the git repositories
